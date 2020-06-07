1736d6c28b194a4a5dca3595d4773e67f0902c28
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-06-08 00:59:56 +0200

    Cambiar horari/1
    Ahora usa la funcion horesDiaries/1 para comprobar que se
    cumplen las restricciones para el horario de un día. Así se
    utiliza un diseño descendente porque las restricciones se
    comprueban en un predicado independiente.

--- horaris.pl
+++ horaris.pl
@@ -9,50 +9,38 @@ assignatura(programacio).
 assignatura(fisica).
 
 % Genera un horari per un dia genèric
-horari([A1,A2,A3,A4,A5]):-
+horari(L):-
     assignatura(A1),
     assignatura(A2),
     assignatura(A3),
     assignatura(A4),
     assignatura(A5),
     append([A1,A2,A3,A4,A5],[],L),
-    % comptarà que en un dia no es facin més de tres hores d'una assignatura
-    comptar(L,N1,estadistica),     
-    comptar(L,N2,algebra),
-    comptar(L,N3,computadors),
-    comptar(L,N4,programacio),
-    comptar(L,N5,fisica),
-    N1=<3,N2=<3,N3=<3,N4=<3,N5=<3,
-    % a més, mirará que cap assignatura tengui 3 hores seguides
-    not(tresSeguides(L,estadistica)),
-    not(tresSeguides(L,algebra)),
-    not(tresSeguides(L,computadors)),
-    not(tresSeguides(L,programacio)),
-    not(tresSeguides(L,fisica)).
+    horesDiaries(L).
 
 horariGeneracio:-
     % Dilluns
     horari([DL1,DL2,DL3,DL4,DL5]),
     % àlgebra pot a partir de la quarta hora (18:30)
-    not(member(algebra,[DL1,DL2,DL3])),   
-    % els dilluns, computadors ha de ser a partir de la quarta hora (18:30)              
-    not(member(computadors,[DL1,DL2,DL3])),   
-    % els dilluns, física no pot ser abans de la tercera hora (17:30)         
-    not(member(fisica,[DL3,DL4,DL5])),                  
+    not(member(algebra,[DL1,DL2,DL3])),
+    % els dilluns, computadors ha de ser a partir de la quarta hora (18:30)
+    not(member(computadors,[DL1,DL2,DL3])),
+    % els dilluns, física no pot ser abans de la tercera hora (17:30)
+    not(member(fisica,[DL3,DL4,DL5])),
     % altre manera d'interpretar l'enunciat:
-    % not(member(fisica,[DL4,DL5])),                    
-    
+    % not(member(fisica,[DL4,DL5])),
+
     % Dimarts
     horari([DM1,DM2,DM3,DM4,DM5]),
     % àlgebra pot a partir de la quarta hora (18:30h)
-    not(member(algebra,[DM1,DM2,DM3])),   
-    % computadors no es pot fer els dimarts              
-    not(member(computadors,[DM1,DM2,DM3,DM4,DM5])),     
+    not(member(algebra,[DM1,DM2,DM3])),
+    % computadors no es pot fer els dimarts
+    not(member(computadors,[DM1,DM2,DM3,DM4,DM5])),
     % programació no es pot fer els dimarts
-    not(member(programacio,[DM1,DM2,DM3,DM4,DM5])),    
-    % els dimarts, física no pot ser abans de la segona hora (16:30h) 
-    not(member(fisica,[DM2,DM3,DM4,DM5])),   
-    % altre manera d'interpretar l'enunciat:           
+    not(member(programacio,[DM1,DM2,DM3,DM4,DM5])),
+    % els dimarts, física no pot ser abans de la segona hora (16:30h)
+    not(member(fisica,[DM2,DM3,DM4,DM5])),
+    % altre manera d'interpretar l'enunciat:
     % not(member(fisica,[DM3,DM4,DM5])),
 
     % Dimecres
@@ -86,9 +74,9 @@ horari:-comptador(X),nl,nl,write("En total hi ha "),write(X),write(" solucions")
     write("En total hi ha "),write(X),write(" solucions").
 
 % Per comprovar les restriccions de l'horari d'un dia
-restriccionsDiaries(L):-
-    % aquí comptarà que en un dia no es puguin fer més de tres hores d'una assignatura
-    comptar(L,N1,estadistica),     
+horesDiaries(L):-
+    % comptarà que en un dia no es facin més de tres hores d'una assignatura
+    comptar(L,N1,estadistica),
     comptar(L,N2,algebra),
     comptar(L,N3,computadors),
     comptar(L,N4,programacio),
