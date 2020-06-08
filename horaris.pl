2df50cbf23f1b89832ed243e897a715e6d358155
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-06-08 12:03:28 +0200

    Reordenar los predicados
    Se han ordenado los predicados siguiendo el orden de
    mayor a menor uso teniendo en cuenta la cantidad de veces
    que se utilizan en la ejecución del programa.

--- horaris.pl
+++ horaris.pl
@@ -8,6 +8,35 @@ assignatura(computadors).
 assignatura(programacio).
 assignatura(fisica).
 
+% Per a assegurar que una assignatura no té 3 hores seguides en un mateix dia
+tresSeguides([X,X,X|_],X).
+tresSeguides([_|L],X):-tresSeguides(L,X).
+
+% Per comptar el nombre d'aparicions d'un element a una llista
+comptar([],0,_).
+comptar([X|L],N,X):-!,comptar(L,N1,X),N is N1+1.
+comptar([_|L],N,X):-comptar(L,N,X).
+
+% Per comprovar les restriccions de l'horari d'un dia
+horesDiaries(L):-
+    %cap assignatura pot tenir 3 hores seguides
+    not(tresSeguides(L,estadistica)),
+    not(tresSeguides(L,algebra)),
+    not(tresSeguides(L,computadors)),
+    not(tresSeguides(L,programacio)),
+    not(tresSeguides(L,fisica)).
+
+% Per comprovar que cap assignatura es fa més de 3 hores a la setmana
+horesSetmanals([L1,L2,L3]):-
+    comptar(L1,NF1,fisica),comptar(L2,NF2,fisica),comptar(L3,NF3,fisica),
+    3 is NF1+NF2+NF3,
+    comptar(L1,NC1,computadors),comptar(L2,NC2,computadors),comptar(L3,NC3,computadors),
+    3 is NC1+NC2+NC3,
+    comptar(L1,NP1,programacio),comptar(L2,NP2,programacio),comptar(L3,NP3,programacio),
+    3 is NP1+NP2+NP3,
+    comptar(L1,NE1,estadistica),comptar(L2,NE2,estadistica),comptar(L3,NE3,estadistica),
+    3 is NE1+NE2+NE3.
+
 % Genera un horari per un dia genèric que compleix la restricció de que cap 
 % assignatura pot tenir 3 hores o més seguides.
 horari([A1,A2,A3,A4,A5]):-
@@ -20,7 +49,7 @@ horari([A1,A2,A3,A4,A5]):-
 
 % Genera un horari per la setmana que compleix les restriccions de tots els 
 % profesors i on cap assignatura té més de 3 hores setmanals.
-horariGeneracio:-
+horaris:-
     % Dimarts
     horari([DM1,DM2,DM3,DM4,DM5]),
     % computadors no es pot fer els dimarts
@@ -69,35 +98,6 @@ horariGeneracio:-
 
 % Per guardar els resultats a un arxiu i el nombre total de solucions
 horari:-retractall(comptador(_)),asserta(comptador(0)),
-    tell('resultats.txt'),horariGeneracio.
+    tell('resultats.txt'),horaris.
 horari:-comptador(X),nl,nl,write("En total hi ha "),write(X),write(" solucions"),told,
     write("En total hi ha "),write(X),write(" solucions").
-
-% Per comprovar les restriccions de l'horari d'un dia
-horesDiaries(L):-
-    %cap assignatura pot tenir 3 hores seguides
-    not(tresSeguides(L,estadistica)),
-    not(tresSeguides(L,algebra)),
-    not(tresSeguides(L,computadors)),
-    not(tresSeguides(L,programacio)),
-    not(tresSeguides(L,fisica)).
-
-% Per comprovar que cap assignatura es fa més de 3 hores a la setmana
-horesSetmanals([L1,L2,L3]):-
-    comptar(L1,NF1,fisica),comptar(L2,NF2,fisica),comptar(L3,NF3,fisica),
-    3 is NF1+NF2+NF3,
-    comptar(L1,NC1,computadors),comptar(L2,NC2,computadors),comptar(L3,NC3,computadors),
-    3 is NC1+NC2+NC3,
-    comptar(L1,NP1,programacio),comptar(L2,NP2,programacio),comptar(L3,NP3,programacio),
-    3 is NP1+NP2+NP3,
-    comptar(L1,NE1,estadistica),comptar(L2,NE2,estadistica),comptar(L3,NE3,estadistica),
-    3 is NE1+NE2+NE3.
-
-% Per a assegurar que una assignatura no té 3 hores seguides en un mateix dia
-tresSeguides([X,X,X|_],X).
-tresSeguides([_|L],X):-tresSeguides(L,X).
-
-% Per comptar el nombre d'aparicions d'un element a una llista
-comptar([],0,_).
-comptar([X|L],N,X):-!,comptar(L,N1,X),N is N1+1.
-comptar([_|L],N,X):-comptar(L,N,X).
